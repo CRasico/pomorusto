f44afdf73ce1efc274fe6e9eac9c8e21dc3588d3
CRasico <rasicoca@gmail.com>
2023-04-01 13:01:38 -0400

    add listing of items and capability to re-draw

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,18 @@ use std::{
     error::Error,
     io::{self, Stdout},
     thread,
-    time::Duration,
+    time::{Duration, self},
 };
 use tui::{
     backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout, Rect},
-    widgets::{Block, Borders, Gauge, Widget},
+    widgets::{Block, Borders, Gauge, Widget, List, ListItem},
     Frame, Terminal, style::{Style, Color, Modifier},
 };
 
 struct PomorustoArgs {
     duration: u64,
+    tasks: Vec<String>
 }
 
 fn main() -> Result<(), io::Error> {
@@ -29,7 +30,18 @@ fn main() -> Result<(), io::Error> {
 
     let mut terminal: Terminal<CrosstermBackend<Stdout>> = create_terminal()?;
 
-    thread::sleep(Duration::from_millis(pomorusto_args.duration));
+    let duration: Duration = Duration::from_secs(pomorusto_args.duration);
+    let timer = time::Instant::now();
+
+    loop {
+        let elapsed: Duration= timer.elapsed();
+        if duration < elapsed {
+            break;
+        }
+
+        let percent_complete: f32 = (elapsed.as_secs_f32() / duration.as_secs_f32()) * 100 as f32 ;
+        draw(&mut terminal, percent_complete as u16, pomorusto_args.tasks.clone())?;
+    }
 
     disable_raw_mode()?;
     execute!(
@@ -42,35 +54,49 @@ fn main() -> Result<(), io::Error> {
     return Ok(());
 }
 
-// TODO: Make This Stateful: https://docs.rs/tui/latest/tui/widgets/trait.StatefulWidget.html
 fn create_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, io::Error> {
     let mut stdout: Stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend: CrosstermBackend<Stdout> = CrosstermBackend::new(stdout);
     let mut terminal: Terminal<CrosstermBackend<Stdout>> = Terminal::new(backend)?;
 
+    return Ok(terminal);
+}
+
+fn draw(terminal: &mut Terminal<CrosstermBackend<Stdout>>, percentage: u16, tasks: Vec<String>) -> Result<(), io::Error>{
     terminal.draw(|frame: &mut Frame<CrosstermBackend<Stdout>>| {
+        let items: Vec<ListItem> =  tasks.into_iter().map(|task: String| {
+            return ListItem::new(task);
+        }).collect();
+
         let chunks: Vec<Rect> = Layout::default()
         .direction(Direction::Vertical)
         .margin(1)
         .constraints(
             [
-                Constraint::Percentage(80),
-                Constraint::Percentage(20),
+                Constraint::Percentage(30),
+                Constraint::Percentage(70),
             ].as_ref()
         )
         .split(frame.size());
-        let block: Block = Block::default().title("Rustodoro Tasks").borders(Borders::ALL);
+
         let gauge: Gauge = Gauge::default().block(
             Block::default()
                 .title("Time Remaining")
                 .borders(Borders::ALL),
-        ).gauge_style(Style::default().fg(Color::Cyan).bg(Color::Black).add_modifier(Modifier::ITALIC)).percent(30);
-        frame.render_widget(block, chunks[0]);
-        frame.render_widget(gauge, chunks[1])
+        ).gauge_style(Style::default().fg(Color::Cyan).bg(Color::Black).add_modifier(Modifier::ITALIC)).percent(percentage);
+
+        let items: List = List::new(items)
+            .block(Block::default().title("Tasks").borders(Borders::ALL))
+            .style(Style::default().fg(Color::White))
+            .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
+            .highlight_symbol(">>");
+
+        frame.render_widget(gauge, chunks[0]);
+        frame.render_widget(items, chunks[1]);
     })?;
 
-    return Ok(terminal);
+    return Ok(());
 }
 
 fn parse_arguments(mut args: Args) -> PomorustoArgs {
@@ -78,5 +104,13 @@ fn parse_arguments(mut args: Args) -> PomorustoArgs {
 
     let duration: u64 = duration_string.parse::<u64>().unwrap();
 
-    return PomorustoArgs { duration };
+    let mut list_items: Vec<String> = vec![];
+    let mut next = args.next();
+    while next.is_some() {
+        let action_item: String = next.unwrap();
+        list_items.push(action_item);
+        next = args.next();
+    }
+
+    return PomorustoArgs { duration, tasks: list_items};
 }
